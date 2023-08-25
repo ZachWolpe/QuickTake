597ae8013af817bd2a22f906777bfb576b2e3c21
ZachWolpe <zachcolinwolpe@gmail.com>
2023-08-25 12:58:39 +0100

    relative path bug fix

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ project_urls = {
 
 setuptools.setup(
     name                            = "quicktake",
-    version                         = "0.0.13",
+    version                         = "0.0.14",
     author                          = "Zach Wolpe",
     author_email                    = "zachcolinwolpe@gmail.com",
     description                     = "Off-the-shelf computer vision ML models. Yolov5, gender and age determination.",
